cb9dec8782de1e292c99d2e636a04ddb119225dc
piassa157 <lindaum98@gmail.com>
2021-03-20 13:49:45 -0300

    [GGP] Feat: Init

---
+++ src/oppjava/OppJava.java
@@ -0,0 +1,21 @@
+
+package oppjava;
+
+/**
+ *
+ * @author Piassa
+ */
+public class OppJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pencil p1 = new Pencil();
+        p1.model = "Bic";
+        p1.color = "blue";
+        p1.status();
+        
+    }
+    
+}
